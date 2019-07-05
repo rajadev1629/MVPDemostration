3d90c48a52e00327cdbd2f906c05a7ce55ceb978
Raja <Raja@retechlabs.com>
2019-07-05 14:14:42 +0530

    Final code

--- app/src/main/java/com/test/logitech/AppInstance.java
+++ app/src/main/java/com/test/logitech/AppInstance.java
@@ -2,13 +2,6 @@ package com.test.logitech;
 
 import android.app.Application;
 
-/**
- * Created by Tarun on 3/15/2016.
- */
-
-/**
- *
- */
 public class AppInstance extends Application {
 
     private static AppInstance instance;
